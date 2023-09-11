7ac45dff521a0b4224ce1f5728adb1bf811d7a61
Lucas Melquiades <lmelquiades@primeit.pt>
2023-09-10 23:26:03 -0300

    vue vfor playground

---
+++ src/components/TheVtj.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import WelcomeItem from './WelcomeItem.vue'
+import DeputyCard from './DeputyCard.vue'
+
+const items = ref([
+  {
+    message: 'Foo',
+    image_path: "https://www.camara.leg.br/internet/deputado/bandep/76874.jpg"
+  },
+  {
+    message: 'Bar',
+    image_path: "https://www.camara.leg.br/internet/deputado/bandep/76874.jpg"
+  }
+])
+
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #heading>LLLLL Vue</template>
+
+    As an independent project, Vue relies on community backing for its sustainability. You can help
+    us by
+    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+  </WelcomeItem>
+
+  <DeputyCard v-for="item in items" name={{item.message}} party={{item.message}} state={{item.message}} >
+      <template #avatar>
+      <img
+        src= {{image_path}}
+        alt="Vue Logo"
+      >
+      </template>
+  </DeputyCard>
+
+  <li v-for="item in items">
+  {{ item.message }}
+  </li>
+
+</template>
+  
---
+++ src/views/VtjView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheVtj from '../components/TheVtj.vue'
+</script>
+
+<template>
+  <main>
+    <TheVtj />
+  </main>
+</template>
